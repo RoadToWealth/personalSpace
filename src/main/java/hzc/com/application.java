449c33d64a408094627cc6a87d94599dfae557d1
HuangZhicheng <huaguohenji@163.com>
2019-10-21 17:31:02 +0800

    个人项目导入

--- src/main/java/hzc/com/application.java
+++ src/main/java/hzc/com/application.java
@@ -1,5 +1,6 @@
 package hzc.com;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date: 2018/7/5 16:16
  */
 @SpringBootApplication
+@MapperScan(basePackages = "club.sscai.userservice.user")
 public class application {
 
     public static void main(String[] args) {
